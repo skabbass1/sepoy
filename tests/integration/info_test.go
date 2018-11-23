f73a398f8fdbd6c0fdeca06dc8fb681a85915765
sabbas <syed.k.abbas1988@gmail.com>
2018-11-23 06:32:14 -0600

    add tests for info command

---
+++ tests/integration/info_test.go
@@ -0,0 +1,54 @@
+package integration
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/skabbass1/sepoy/commands"
+)
+
+func TestMain(m *testing.M) {
+	setUp()
+	code := m.Run()
+	tearDown()
+	os.Exit(code)
+}
+
+func TestTaskInfo(t *testing.T) {
+
+	out, err := commands.TaskInfo(taskName())
+	if err != nil {
+		t.Errorf("%s: %v", out, err)
+	}
+}
+
+func setUp() {
+	err := commands.ScheduleBatchTask(
+		taskName(),
+		[]string{"ls", "-ltr"},
+		map[string]int{
+			"month":   0,
+			"day":     0,
+			"weekday": 1,
+			"hour":    13,
+			"minute":  0,
+		},
+		map[string]string{},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func tearDown() {
+	defer os.Remove(commands.PlistLocation(taskName()))
+	err := commands.UnscheduleTask(taskName())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func taskName() string {
+	return "com.testls"
+}
